e81f087da0fc38926d8bab1259993a985f37f66b
Kevin Leung <kevinresol@gmail.com>
2017-06-16 15:45:10 +0800

    Add prev/next links (closes #1)

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ window.$docsify.externalLinkTarget = '_self';
 window.$docsify.subMaxLevel = 3;
 
 if(!window.$docsify.plugins) window.$docsify.plugins = [];
-window.$docsify.plugins.push(function(hook) {
-	hook.afterEach(function (html) {
+window.$docsify.plugins.push(function(hook, vm) {
+	hook.afterEach(function (html, next) {
 		var repo = window.location.pathname.substr(1); // strip the `/`
 		var path = window.location.hash.substr(1).split('?')[0]; // strip the `#` and anything after `?`
 		if(repo == '')	
@@ -12,9 +12,31 @@ window.$docsify.plugins.push(function(hook) {
 		else
 			repo += '/blob/gh-pages';
 			
-		if(path == '/') path = '/README';
-		path += '.md';
+		path += vm.route.file;
 		
-		return html + '<hr/><footer><a href="https://github.com/haxetink/' + repo + path + '">Edit this page</a></footer>';
+		var footer = ['[Edit this page](https://github.com/haxetink/' + repo + path + ')\n'];
+		
+		function done() {
+			next(html + '\n' + footer.join('\n'));
+		}
+		
+		if(vm.config.loadSidebar) {
+			fetch(vm.config.loadSidebar)
+				.then(function(res) {
+					return res.text();
+				})
+				.then(function(sidebar) {
+					var regex = /\((.*\.md)\)/gi, result, links = [];
+					while ( (result = regex.exec(sidebar)) ) {
+						links.push('/' + result[1]);
+					}
+					var index = links.indexOf(vm.route.file);
+					if(index > 0) footer.push('[< Prev](' + links[index - 1] + ')');
+					if(index < links.length - 1) footer.push('[Next >](' + links[index + 1] + ')');
+					done();
+				});
+		} else {
+			done();
+		}
 	});
 });
